659025a71a8f392cf31f8ab3fd734ca436abf9bf
bhandarirabbie <me.rabinbhandari@gmail.com>
2022-10-08 18:51:05 +0900

    making change in all files and folder

---
+++ DSA Topic/Array/PracticingArray.java
@@ -0,0 +1,13 @@
+package practice.array.java;
+import java.util.Arrays;
+public class PracticingArray {
+    public static void main(String [] args){
+        int[] numbers = {10, 20, 30};
+//    int[] numbers = new int[3];
+//    numbers[0] = 10;
+//    numbers[1] = 20;
+//    numbers[2] = 30;
+        System.out.println(numbers.length);
+    System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ DSA Topic/Array/PracticingArrayClass.java
@@ -0,0 +1,7 @@
+package practice.array.java;
+public class PracticingArrayClass {
+    public static void main(String[] args){
+                Array numbers = new Array(3);
+
+    }
+}
